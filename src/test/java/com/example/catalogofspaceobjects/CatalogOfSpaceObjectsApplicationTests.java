f30f3bfb97e8afdd8a20e8d9eab27612c6fed94d
Pepi <petranka_popova_@abv.bg>
2024-01-17 13:33:40 +0200

    Initial commit

---
+++ src/test/java/com/example/catalogofspaceobjects/CatalogOfSpaceObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogofspaceobjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogOfSpaceObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
